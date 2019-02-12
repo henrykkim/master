aceb8d428d7892d1f4344ecd81f4921f8a883b93
Henry Kim <henrykkim04@gmail.com>
2019-02-11 23:29:09 -0500

    Test

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: "Henry Kim's Master Project",
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <div className="Top">
+      <div className="TopGroup">
+        <h1>Korean Food, <br />Korean People</h1>
+        <p>Atlanta has one of the biggest Korean immigrant population in the U.S. Here are interviews of five Korean immigrants who own Korean restaurants in Atlanta. </p>
+      </div>
+    </div>
+  </div>
+)
+
+export default IndexPage
