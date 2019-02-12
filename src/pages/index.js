41fed498d9f30e7a0a0d0bcf154f6ba11d9413eb
Henry Kim <henrykkim04@gmail.com>
2019-02-11 23:56:37 -0500

    Card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Card from '../components/Card';
 
 const IndexPage = () => (
   <div>
@@ -9,7 +10,18 @@ const IndexPage = () => (
         <p>Atlanta has one of the biggest Korean immigrant population in the U.S. Here are interviews of five Korean immigrants who own Korean restaurants in Atlanta. </p>
       </div>
     </div>
+    <div className="Cards">
+      <div className="CardGroup">
+        <Card 
+        title ="Design System" 
+        text="10 Sections" 
+        image={require('../images/yet_tuh1.jpg')}  
+        />
+      </div>
+    </div>
   </div>
+
+  
 )
 
 export default IndexPage
